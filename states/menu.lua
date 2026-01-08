253a7e58cfdd6727cc85d2313b5da42107e89d2e
Matthew Poole Chicano <matthew.poole485@gmail.com>
2026-01-08 08:32:09 -0500

    Updated menu state with virtual.lua and globals.lua files

--- states/menu.lua
+++ states/menu.lua
@@ -1,11 +1,45 @@
+-- =============================================================================
+-- states/menu.lua
+-- -----------------------------------------------------------------------------
+
+local G = require("lib.globals")
+local Text = require("ui.text")
 local menu = {}
+local title
+local menuCanvas
 
-function menu.load() end
+function menu.load()
+	menuCanvas = love.graphics.newCanvas(G.virtual.WIDTH, G.virtual.HEIGHT)
+	G.virtual.scaleScreen()
+	title = Text.new("Battle Tactics Arena", 100, 100)
+end
 
-function menu.update(dt) end
+function menu.update(dt)
+	G.virtual.scaleScreen()
+end
 
 function menu.draw()
-	love.graphics.print("menu")
+	love.graphics.setCanvas(menuCanvas)
+	love.graphics.clear(0.3, 0.4, 0.4)
+	title:draw()
+	love.graphics.setCanvas()
+	if G.virtual.scale and G.virtual.scale > 0 then
+		love.graphics.draw(
+		  menuCanvas,
+		  G.virtual.translateX,
+		  G.virtual.translateY,
+		  0,
+		  G.virtual.scale,
+		  G.virtual.scale
+		)
+	end
+end
+
+function menu.keyreleased(key)
+	if key == G.controls.ESC then
+		love.event.quit()
+		return
+	end
 end
 
 return menu
